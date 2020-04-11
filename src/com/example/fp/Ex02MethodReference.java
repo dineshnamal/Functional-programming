0534ba8d1a2627d9bb9a732797ee752ae4342574
user <namal3000@gmail.com>
2020-04-11 19:33:17 +1000

    Ex02MethodReference - Filter numbers Ex01MethodReference - AddDescription

--- src/com/example/fp/Ex02MethodReference.java
+++ src/com/example/fp/Ex02MethodReference.java
@@ -3,23 +3,23 @@ package com.example.fp;
 import java.util.List;
 
 /**
- * Print Numbers in the List
+ * Print Even numbers
  * @author Namal
  *
  */
-public class Ex01MethodReference {
+
+public class Ex02MethodReference {
 	public static void main(String[] args) {
 		printAllNumbersInListFunctional(List.of(1, 22, 14, 15, 7, 66));
 
 	}
 
-	private static void print(int number) {
-		System.out.println(number);
-	}
+
 
 	private static void printAllNumbersInListFunctional(List<Integer> numbers) {
 		numbers.stream() // Convert numbers into Stream of elements
-		.forEach(Ex01MethodReference::print);//For each element in the stream calling print method in the current class
+		.filter(s->s%2 == 0)
+		.forEach(System.out::println);//This is improvement of Ex01 MethodReference not using print method
 	}
 
 }
