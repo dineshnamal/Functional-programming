a36aa8f6ee635644385bcc08aca567c1dd9055ec
user <namal3000@gmail.com>
2020-04-11 19:25:36 +1000

    Ex01MethodReference - Print Numbers with FP

--- src/com/example/fp/Ex01MethodReference.java
+++ src/com/example/fp/Ex01MethodReference.java
@@ -1,12 +1,20 @@
 package com.example.fp;
 
+import java.util.List;
+
 public class Ex01MethodReference {
 	public static void main(String[] args) {
-		
-		
-		
+		printAllNumbersInListFunctional(List.of(1, 22, 14, 15, 7, 66));
 
-		
+	}
+
+	private static void print(int number) {
+		System.out.println(number);
+	}
+
+	private static void printAllNumbersInListFunctional(List<Integer> numbers) {
+		numbers.stream() // Convert numbers into Stream of elements
+		.forEach(Ex01MethodReference::print);//For each element in the stream calling print method in the current class
 	}
 
 }
